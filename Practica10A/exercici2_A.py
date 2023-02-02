02e3367035942dc6d8a94ba1c207f8560ff7e909
ilich <itovar22@jaumebalmes.net>
2023-02-02 19:02:56 +0100

    parte A

---
+++ Practica10A/exercici2_A.py
@@ -0,0 +1,5 @@
+myDict = {'prova':18, 'Roger':'Sobrino', 'Edad':20, 'prova2':25, 'Tovar':'calderon','edad2':15}
+print(len(myDict))#longitud del diccionari
+print(myDict.values())#valores del diccionari
+print(myDict.popitem())#extració de l'ultim item
+print(myDict)
